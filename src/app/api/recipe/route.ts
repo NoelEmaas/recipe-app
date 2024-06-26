b4d3c826c65226fc06fd07c5562ea3a6bcd97896
NoelEmaas <nemaas.dev@gmail.com>
2024-06-26 21:14:41 +0800

    feat: added post request to api to add new recipe

--- src/app/api/recipe/route.ts
+++ src/app/api/recipe/route.ts
@@ -9,3 +9,14 @@ export async function GET () {
 
   return NextResponse.json(recipes);
 }
+
+export async function POST (request: Request) {
+  const recipe = await request.json() as Recipe;
+  const fileUtility = new FileUtility<Recipe>("recipe");
+  const recipes = await fileUtility.read();
+  
+  recipes.push(recipe);
+  await fileUtility.write(recipes);
+
+  return NextResponse.json(recipe, { status: 201 });
+}

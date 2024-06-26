5774c434d2fd3d2559159de48387cdcc798482bb
NoelEmaas <nemaas.dev@gmail.com>
2024-06-27 04:43:56 +0800

    feat: added api route to update user data

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -19,3 +19,22 @@ export async function GET (request: Request, context: { params: { id: string } }
 
   return NextResponse.json(user);
 }
+
+export async function PUT (request: Request, context: { params: { id: string } }) {
+  const { id } = context.params;
+  const newUser = await request.json() as User;
+  const users = await fileUtility.read();
+  const user = users.findIndex((user: User) => user.id === Number(id));
+
+  if (user === -1) {
+    return NextResponse.json(
+      { error: "User not found" },
+      { status: 404 }
+    );
+  }
+
+  users[user] = newUser;
+  await fileUtility.write(users);
+
+  return NextResponse.json(newUser);
+}

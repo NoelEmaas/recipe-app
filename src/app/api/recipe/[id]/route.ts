2985fac425c7a7cb38a6b86216fd0337d04f9da3
NoelEmaas <nemaas.dev@gmail.com>
2024-06-26 22:02:28 +0800

    feat: added api route for editing the details of specific recipe

--- src/app/api/recipe/[id]/route.ts
+++ src/app/api/recipe/[id]/route.ts
@@ -19,3 +19,24 @@ export async function GET (request: Request, context: { params: { id: string } }
 
   return NextResponse.json(recipe);
 }
+
+export async function PUT (request: Request, context: { params: { id: string } }) {
+  const { id } = context.params;
+  const recipe = await request.json() as Recipe;
+
+  const fileUtility = new FileUtility<Recipe>("recipe");
+  const recipes = await fileUtility.read();
+  const index = recipes.findIndex((recipe: Recipe) => recipe.id === Number(id));
+
+  if (index === -1) {
+    return NextResponse.json(
+      { error: "Recipe not found" },
+      { status: 404 }
+    );
+  }
+
+  recipes[index] = recipe;
+  await fileUtility.write(recipes);
+
+  return NextResponse.json(recipe);
+}

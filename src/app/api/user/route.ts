4fab834b132ef4615af5a3580a6f81b4f1a16b1a
NoelEmaas <nemaas.dev@gmail.com>
2024-06-27 04:33:53 +0800

    feat: added api route to get all users

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,3 +1,4 @@
+import { NextResponse } from "next/server";
 import { User } from "@/types/user.type";
 
 import FileUtility from "@/utils/fileUtility";
@@ -5,5 +6,7 @@ import FileUtility from "@/utils/fileUtility";
 const fileUtility = new FileUtility<User>("user");
 
 export async function GET () {
+  const users = await fileUtility.read();
   
+  return NextResponse.json(users);
 }

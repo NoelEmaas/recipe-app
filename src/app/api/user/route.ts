2c6302640f2e0c26bb8ee117bc6151f685c60373
NoelEmaas <nemaas.dev@gmail.com>
2024-06-27 03:00:46 +0800

    feat: added user type

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,9 @@
+import { User } from "@/types/user.type";
+
+import FileUtility from "@/utils/fileUtility";
+
+const fileUtility = new FileUtility<User>("user");
+
+export async function GET () {
+  
+}

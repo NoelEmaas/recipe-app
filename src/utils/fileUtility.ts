f5c3a6780a3b6464877c08054cda7b110b3eb098
NoelEmaas <nemaas.dev@gmail.com>
2024-06-26 22:33:05 +0800

    feat: added checker functions to check if file and folder exists

--- src/utils/fileUtility.ts
+++ src/utils/fileUtility.ts
@@ -8,12 +8,33 @@ class FileUtility<T> {
 
   private filePath: string;
 
+  private checkPath = async () => {
+    try {
+      await fs.access(path.join(process.cwd(), 'data'));
+    } catch (error) {
+      await fs.mkdir(path.join(process.cwd(), 'data'));
+    }
+  }
+
+  private checkFile = async () => {
+    try {
+      await fs.access(this.filePath);
+    } catch (error) {
+      await fs.writeFile(this.filePath, '[]');
+    }
+  }
   read = async () => {
+    await this.checkPath();
+    await this.checkFile();
+
     const data = await fs.readFile(this.filePath, 'utf-8');
     return JSON.parse(data);
   }
 
   write = async (data: T) => {
+    await this.checkPath();
+    await this.checkFile();
+    
     await fs.writeFile(this.filePath, JSON.stringify(data));
   }
 }

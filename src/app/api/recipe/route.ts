5b5f3c45e0016c84960988d6c27cfffe93e1842f
NoelEmaas <nemaas.dev@gmail.com>
2024-06-26 20:56:56 +0800

    feat: added api route for querying all recipes

--- src/app/api/recipe/route.ts
+++ src/app/api/recipe/route.ts
@@ -1,5 +1,11 @@
 import { NextResponse } from "next/server"
+import { Recipe } from "@/types/recipe.type";
+
+import FileUtility from "@/utils/fileUtility";
 
 export async function GET () {
-  return NextResponse.json({ message: "Hello, World!" });
+  const fileUtility = new FileUtility<Recipe>("recipe");
+  const recipes = await fileUtility.read();
+
+  return NextResponse.json(recipes);
 }

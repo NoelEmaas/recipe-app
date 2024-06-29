c26b6af565db71d7bc56a96a62c4d65d3f75bd4e
NoelEmaas <nemaas.dev@gmail.com>
2024-06-29 17:02:02 +0800

    feat: added hover effect on card

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -4,11 +4,12 @@ import { Recipe } from "@/types/recipe.type"
 const RecipeCard = (recipe: Recipe) => {
   return (
     <Link href={`/recipe/${recipe.id}`} className="sm:block flex gap-x-4 border p-4 rounded-xl sm:h-full h-[140px] items-center group">
-      <div className="border rounded-lg sm:mb-4 sm:w-full sm:h-[200px] w-[150px] h-full overflow-hidden">
+      <div className="border rounded-lg sm:mb-4 sm:w-full sm:h-[200px] w-[150px] h-full overflow-hidden sm:block hidden">
         <img src={recipe.image} alt="" className="w-full h-full transition-transform duration-300 transform group-hover:scale-110"/>
       </div>
+      <img src={recipe.image} alt="" className="border rounded-lg sm:mb-4 sm:w-full sm:h-[200px] w-[150px] h-full sm:hidden"/>
       <div>
-        <h2 className="font-bold mb-2 line-clamp-1 group-hover:underline">{recipe.name}</h2>
+        <h2 className="font-bold mb-2 line-clamp-2 group-hover:underline">{recipe.name}</h2>
         <p className="text-sm text-gray-600 sm:line-clamp-3 line-clamp-2 sm:mb-4">{recipe.description}</p>
       </div>
     </Link>

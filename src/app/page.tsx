2993a4a92ec563259f4340234effbb0003c1e2cd
NoelEmaas <nemaas.dev@gmail.com>
2024-06-30 12:47:27 +0800

    feat: added skeleton loader in hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { 
   Categories,
   Hero, 
@@ -6,11 +8,18 @@ import {
   Title
 } from "@/components";
 
+import { useFetchRecipes } from "@/hooks/recipe.hook";
+
 const Home = () => {
+  const { data, isPending, error } = useFetchRecipes();
+  
   return (
     <main >
       <div className="container mx-auto px-4">
-        <Hero />
+        <Hero 
+          recipes={ data! } 
+          isPending={ isPending }
+        />
         <Title title="Perfect for this Season" />
         <Seasonal />
         <Title title="Categories" />
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,43 +2,43 @@
 
 import { useState, useEffect } from "react";
 import { Playfair_Display } from "next/font/google";
+import { Image, MoveRight  } from 'lucide-react';
 import { Recipe } from "@/types/recipe.type";
-import { MoveRight } from 'lucide-react';
 
 import Link from "next/link";
 
 const playfair_display = Playfair_Display({ weight: "700", subsets: ["latin"] });
 
-const Hero = () => {
-  const [recipes, setRecipes] = useState<Recipe[]>([]);
+interface HeroProps {
+  recipes?: Recipe[];
+  isPending: boolean;
+}
+
+const Hero = ({ recipes, isPending }: HeroProps) => {
   const [currentRecipeIndex, setCurrentRecipeIndex] = useState(0);
   const [isTransitioning, setIsTransitioning] = useState(false);
 
   useEffect(() => {
-    const fetchRecipes = async () => {
-      const response = await fetch("/api/recipe");
-      const data = await response.json();
-      setRecipes(data);
+    if (recipes && recipes.length > 0) {
+      const interval = setInterval(() => {
+        setIsTransitioning(true);
+        setTimeout(() => {
+          setCurrentRecipeIndex((prevIndex) => 
+            (prevIndex + 1) % recipes.length
+          );
+          setIsTransitioning(false);
+        }, 500);
+      }, 5000);
+
+      return () => clearInterval(interval);
     }
+  }, [recipes]);
 
-    fetchRecipes();
-  }, []);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setIsTransitioning(true);
-      setTimeout(() => {
-        setCurrentRecipeIndex((prevIndex) => 
-          (prevIndex + 1) % recipes.length
-        );
-        setIsTransitioning(false);
-      }, 500);
-    }, 5000);
-
-    return () => clearInterval(interval);
-  }, [recipes.length]);
-
-  const currentRecipe = recipes[currentRecipeIndex];
+  if (isPending) {
+    return <HeroSkeleton />
+  }
+  
+  const currentRecipe = recipes![currentRecipeIndex];
 
   return (
     <div className="animate-fade-up-enter w-full flex xl:h-[600px] lg:h-[400px] sm:h-[350px] h-[250px] items-center mb-10 rounded-xl p-0 lg:mt-0 mt-10">
@@ -72,6 +72,32 @@ const Hero = () => {
           View Recipe
           <MoveRight size={24} />
         </Link>
+      </div>
+    </div>
+  )
+}
+
+const HeroSkeleton = () => {
+  return (
+    <div className="animate-fade-up-enter w-full flex xl:h-[600px] lg:h-[400px] sm:h-[350px] h-[250px] items-center mb-10 rounded-xl p-0 lg:mt-0 mt-10">
+      <div className="pr-2 lg:w-[65%] w-full xl:h-[600px] lg:h-[400px] sm:h-[350px] h-[250px] relative rounded-xl">
+        <div className="h-full w-full lg:rounded-l-xl rounded-xl animate-pulse bg-gray-100 animate-pulse flex flex-col items-center justify-center">
+          <Image color="#9CA3AF" className="w-[100px] h-[100px] mb-4" strokeWidth={1}/>
+          <p className="italic text-gray-400">Loading Recipe Image ...</p>
+        </div>
+        <div className="lg:hidden block hidde absolute z-10 bottom-4 text-white px-6 pb-4 animate-pulse">
+          <div className="bg-gray-900 h-6 w-1/2 mb-4"/>
+          <div className="bg-gray-900 h-4 w-3/4"/>
+          <div className="bg-gray-900 h-4 w-3/4"/>
+          <div className="bg-gray-900 h-4 w-3/4"/>
+        </div>
+      </div>
+      <div className="lg:flex flex-col hidden p-10 h-full w-[35%] bg-slate-50 rounded-r-xl justify-center border gap-y-3">
+        <div className="bg-gray-300 h-8 w-1/2 rounded-lg animate-pulse"/>
+        <div className="bg-gray-300 h-8 w-1/2 mb-4 rounded-lg animate-pulse"/>
+        <div className="bg-gray-200 h-4 w-3/4 rounded-lg animate-pulse"/>
+        <div className="bg-gray-200 h-4 w-3/4 rounded-lg animate-pulse"/>
+        <div className="bg-gray-200 h-4 w-3/4 rounded-lg animate-pulse"/>
       </div>
     </div>
   )

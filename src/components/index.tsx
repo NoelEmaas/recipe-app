26116dc42f0d500776c31871637126d70895c619
NoelEmaas <nemaas.dev@gmail.com>
2024-07-01 18:18:53 +0800

    fix: removed unused components

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -6,9 +6,7 @@ import Footer from './Footer';
 import Header from './Header';
 import Hero from './Hero';
 import RecipeList from './RecipeList';
-import RecipeCard from './RecipeCard';
 import Seasonal from './Seasonal';
-import SeasonalCard from './SeasonalCard';
 import Title from './Title';
 
 export {
@@ -19,8 +17,6 @@ export {
   Header,
   Hero,
   RecipeList,
-  RecipeCard,
   Seasonal,
-  SeasonalCard,
   Title
 }
